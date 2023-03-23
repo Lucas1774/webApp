470fa794bb65ca03fc38fa0904609e2fdcafa909
Lucas1774 <lucas.115@hotmail.com>
2023-03-23 03:10:29 +0100

    Implement DNS logic from (hidden) IP addresses

--- server/src/main/java/com/lucas/server/components/Config.java
+++ server/src/main/java/com/lucas/server/components/Config.java
@@ -1,15 +1,16 @@
 package com.lucas.server.components;
 
+import java.io.IOException;
 import java.util.Arrays;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
-import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -17,28 +18,32 @@ import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class Config {
-
+    @Value("${spring.IPAddress.privateAddress}")
+    private String privateAddress;
+    @Value("${spring.IPAddress.publicAddress}")
+    private String publicAddress;
+    
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
+    public CorsConfigurationSource corsConfigurationSource() throws IOException {
+        
+
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://192.168.1.134:3000"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://" + privateAddress + ":3000", "http://" + publicAddress + ":3000"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
     @Bean
-    @DependsOn ("corsConfigurationSource")
-    public FilterRegistrationBean<CorsFilter> corsFilter(CorsConfigurationSource corsConfigurationSource) {
-        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource);
-        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(corsFilter);
-        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
-        return bean;
+    @DependsOn("corsConfigurationSource")
+    public CorsFilter corsFilter() throws IOException {
+        return new CorsFilter(corsConfigurationSource());
     }
 
     @Bean
+    @DependsOn("corsFilter")
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf()
@@ -46,6 +51,7 @@ public class Config {
             .authorizeHttpRequests()
                 .anyRequest().authenticated()
             .and()
+            .addFilterBefore(corsFilter(), UsernamePasswordAuthenticationFilter.class)
             .httpBasic()
             .and()
             .sessionManagement()
@@ -53,3 +59,4 @@ public class Config {
         return http.build();
     }
 }
+
