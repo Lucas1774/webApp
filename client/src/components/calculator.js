9c448a62304dde9e48f8f86c1a8bc7cc954e57a3
L <L>
2023-03-28 10:13:33 +0200

    Style

--- client/src/components/calculator.js
+++ client/src/components/calculator.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
 import {post, get} from "../components/api";
-import { Form, FormControl, Button, Row, Col, Container } from 'react-bootstrap';
+import { Form, FormControl, Button, Row,  Container } from 'react-bootstrap';
 import "../assets/styles/calculator.css"
 
 function Calculator() {
@@ -65,39 +65,39 @@ function Calculator() {
         <FormControl ref={inputRef} value={input} onChange={handleKeyDown} />
       </Form>
       <Row className="first">
-        <Col><Button onClick={handleClick} value="(">{'('}</Button></Col>
-        <Col><Button onClick={handleClick} value=")">{')'}</Button></Col>
-        <Col><Button onClick={handleClick} value="sqrt">{'\u221A'}</Button></Col>
-        <Col><Button onClick={handleClick} value="^">^</Button></Col>
-        <Col><Button onClick={handleClick} value="log">log</Button></Col>
+        <Button onClick={handleClick} value="(">{'('}</Button>
+        <Button onClick={handleClick} value=")">{')'}</Button>
+        <Button onClick={handleClick} value="sqrt">{'\u221A'}</Button>
+        <Button onClick={handleClick} value="^">^</Button>
+        <Button onClick={handleClick} value="log">log</Button>
       </Row>
       <Row >
-        <Col><Button onClick={handleClick} value="7">7</Button></Col>
-        <Col><Button onClick={handleClick} value="8">8</Button></Col>
-        <Col><Button onClick={handleClick} value="9">9</Button></Col>  
-        <Col><Button onClick={handleDelete}>{'\u2190'}</Button></Col>  
-        <Col><Button onClick={handleClear}>C</Button></Col>
+        <Button onClick={handleClick} value="7">7</Button>
+        <Button onClick={handleClick} value="8">8</Button>
+        <Button onClick={handleClick} value="9">9</Button>  
+        <Button onClick={handleDelete}>{'\u2190'}</Button>  
+        <Button onClick={handleClear}>C</Button>
       </Row>
       <Row>
-        <Col><Button onClick={handleClick} value="4">4</Button></Col>
-        <Col><Button onClick={handleClick} value="5">5</Button></Col>
-        <Col><Button onClick={handleClick} value="6">6</Button></Col>  
-        <Col><Button onClick={handleClick} value="*">*</Button></Col>  
-        <Col><Button onClick={handleClick} value="/">/</Button></Col>  
+        <Button onClick={handleClick} value="6">6</Button>  
+        <Button onClick={handleClick} value="5">5</Button>
+        <Button onClick={handleClick} value="4">4</Button>
+        <Button onClick={handleClick} value="*">*</Button>  
+        <Button onClick={handleClick} value="/">/</Button>  
       </Row>
       <Row>
-        <Col><Button onClick={handleClick} value="1">1</Button></Col>
-        <Col><Button onClick={handleClick} value="2">2</Button></Col>
-        <Col><Button onClick={handleClick} value="3">3</Button></Col>
-        <Col><Button onClick={handleClick} value="+">+</Button></Col>
-        <Col><Button onClick={handleClick} value="-">-</Button></Col>
+        <Button onClick={handleClick} value="1">1</Button>
+        <Button onClick={handleClick} value="2">2</Button>
+        <Button onClick={handleClick} value="3">3</Button>
+        <Button onClick={handleClick} value="+">+</Button>
+        <Button onClick={handleClick} value="-">-</Button>
       </Row>
       <Row>
-        <Col><Button onClick={handleClick} value="0">0</Button></Col>
-        <Col><Button onClick={handleClick} value=".">.</Button></Col>
-        <Col><Button onClick={handleClick} value="*10^">EXP</Button></Col>
-        <Col><Button onClick={handleReceive}>Ans</Button></Col>
-        <Col><Button type="submit" variant="success" onClick={handleSubmit}>=</Button></Col>
+        <Button onClick={handleClick} value="0">0</Button>
+        <Button onClick={handleClick} value=".">.</Button>
+        <Button onClick={handleClick} value="*10^">EXP</Button>
+        <Button onClick={handleReceive}>Ans</Button>
+        <Button type="submit" variant="success" onClick={handleSubmit}>=</Button>
       </Row>
     </Container>
   );
