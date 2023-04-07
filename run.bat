a313f5cc05bb4a1afe72042131047d5f8a61cb92
L <L>
2023-04-07 22:03:56 +0200

    final commit

--- run.bat
+++ run.bat
@@ -11,11 +11,11 @@ netsh advfirewall set privateprofile state off
 git switch main
 
 cd server
-start /b cmd /c "mvnw spring-boot:run > server.log 2>&1"
+start /b cmd /c "mvnw spring-boot:run"
 cd ..
 
 cd client
-start /b cmd /c "npm start > client.log 2>&1"
+start /b cmd /c "npm start"
 cd ..
 
 echo Press any key to exit.
@@ -23,5 +23,5 @@ echo Press any key to exit.
 pause >nul
 netsh advfirewall set privateprofile state on
 
-taskkill /f /t /im java.exe >nul 2>&1
-taskkill /f /t /im node.exe >nul 2>&1
+taskkill /f /t /im java.exe
+taskkill /f /t /im node.exe
