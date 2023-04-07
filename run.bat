3b166cb48674ef30806fcbfc66f4c4fb2a4fbee6
Lucas1774 <lucas.115@hotmail.com>
2023-04-07 22:00:48 +0200

    debug

--- run.bat
+++ run.bat
@@ -4,21 +4,24 @@ if "%1"=="ELEV" (goto run) else (powershell start -verb runas '%0' ELEV & exit /
 
 :run
 
-cd /d "%~dp0"
+cd "%~dp0"
 
 netsh advfirewall set privateprofile state off
 
 git switch main
 
-start /B /k cmd /c "cd server && mvnw spring-boot:run"
+cd server
+start /b cmd /c "mvnw spring-boot:run > server.log 2>&1"
+cd ..
 
-start /B /k cmd /c "cd client && npm start"
+cd client
+start /b cmd /c "npm start > client.log 2>&1"
+cd ..
 
 echo Press any key to exit.
 
-
 pause >nul
-
-taskkill /f /im java.exe >nul 2>&1
-taskkill /f /im node.exe >nul 2>&1
 netsh advfirewall set privateprofile state on
+
+taskkill /f /t /im java.exe >nul 2>&1
+taskkill /f /t /im node.exe >nul 2>&1
