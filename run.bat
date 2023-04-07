3807e5f7b808cbeb18377d5a5018ab2fd149084d
Lucas1774 <lucas.115@hotmail.com>
2023-04-07 22:25:08 +0200

    add firewall settings automation

--- run.bat
+++ run.bat
@@ -1,27 +1,15 @@
 @echo off
-
 if "%1"=="ELEV" (goto run) else (powershell start -verb runas '%0' ELEV & exit /b)
 
 :run
-
 cd "%~dp0"
-
 netsh advfirewall set privateprofile state off
-
 git switch main
-
-cd server
-start /b cmd /c "mvnw spring-boot:run"
-cd ..
-
-cd client
-start /b cmd /c "npm start"
-cd ..
+start /b cmd /c "cd server && mvnw spring-boot:run > nul 2>&1"
+start /b cmd /c "cd client && npm start > nul 2>&1"
 
 echo Press any key to exit.
-
 pause >nul
 netsh advfirewall set privateprofile state on
-
-taskkill /f /t /im java.exe
-taskkill /f /t /im node.exe
+taskkill /f /t /im java.exe > nul 2>&1
+taskkill /f /t /im node.exe > nul 2>&1
