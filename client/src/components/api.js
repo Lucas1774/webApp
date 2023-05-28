c98bcb83ca0f9ed130a304aa24061a3b53af31d4
Lucas1774 <lucas.115@hotmail.com>
2023-05-28 02:03:52 +0200

    Fix typo

--- client/src/components/api.js
+++ client/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const isLocal = window.location.hostname === 'localhost' || window.location.hostname === process.env.REACT_APP_PRIVATE_ADDRESS;
-const BASE_URL = isLocal ? `https://${process.env.REACT_APP_PRIVATE_ADDRESS}:8443/api` : `https://${process.env.REACT_APP_PUBLIC_ADDRESS}:8443/api`;
+const BASE_URL = isLocal ? `https://${process.env.REACT_APP_PRIVATE_ADDRESS}:8043/api` : `https://${process.env.REACT_APP_PUBLIC_ADDRESS}:8043/api`;
 const auth = {username: 'user', password: 'password'};
 const config = {
   headers: {
