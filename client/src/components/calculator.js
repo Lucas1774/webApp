7e1676528295e9c80bec33d3025c3b5b0d0cf9cc
L <L>
2023-11-22 19:48:34 +0100

    Remove dupped calculator.js Format calculator.js

--- client/src/components/calculator.js
+++ client/src/components/calculator.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import {post, get} from "../components/api";
-import { Form, FormControl, Button, Row,  Container } from 'react-bootstrap';
+import { post, get } from "../components/api";
+import { Form, FormControl, Button, Row, Container } from 'react-bootstrap';
 import "../assets/styles/calculator.css"
 
 function Calculator() {
@@ -24,8 +24,8 @@ function Calculator() {
   function handleKeyDown(event) {
     event.preventDefault();
     setInput(event.target.value);
-  }  
-  
+  }
+
   function handleClick(event) {
     event.preventDefault();
     setInput(input + event.target.value);
@@ -34,30 +34,30 @@ function Calculator() {
   function handleDelete() {
     setInput(input.substring(0, input.length - 1));
   }
-  
+
   function handleClear() {
     setInput("");
   }
-    
+
   function handleSubmit(event) {
     event.preventDefault();
     post('/ans', input)
-    .then(response => {
-      setInput(response.data.toString());
-    })
-    .catch(error => {
-      alert("Error sending data: " + error.message);
-    });
+      .then(response => {
+        setInput(response.data.toString());
+      })
+      .catch(error => {
+        alert("Error sending data: " + error.message);
+      });
   }
 
   function handleReceive() {
     get('/ans')
-    .then(response => {
-      setInput (input + response.data.toString());
-    })
-    .catch(error => {
-      alert("Error receiving data: " + error.message);
-    });
+      .then(response => {
+        setInput(input + response.data.toString());
+      })
+      .catch(error => {
+        alert("Error receiving data: " + error.message);
+      });
   }
   return (
     <Container className="calculator">
@@ -74,16 +74,16 @@ function Calculator() {
       <Row >
         <Button onClick={handleClick} value="7">7</Button>
         <Button onClick={handleClick} value="8">8</Button>
-        <Button onClick={handleClick} value="9">9</Button>  
-        <Button onClick={handleDelete}>{'\u2190'}</Button>  
+        <Button onClick={handleClick} value="9">9</Button>
+        <Button onClick={handleDelete}>{'\u2190'}</Button>
         <Button onClick={handleClear}>C</Button>
       </Row>
       <Row>
-        <Button onClick={handleClick} value="6">6</Button>  
+        <Button onClick={handleClick} value="6">6</Button>
         <Button onClick={handleClick} value="5">5</Button>
         <Button onClick={handleClick} value="4">4</Button>
-        <Button onClick={handleClick} value="*">*</Button>  
-        <Button onClick={handleClick} value="/">/</Button>  
+        <Button onClick={handleClick} value="*">*</Button>
+        <Button onClick={handleClick} value="/">/</Button>
       </Row>
       <Row>
         <Button onClick={handleClick} value="1">1</Button>
@@ -101,5 +101,5 @@ function Calculator() {
       </Row>
     </Container>
   );
-}    
+}
 export default Calculator;
