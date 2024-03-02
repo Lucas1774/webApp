afaa0de864236f2cd5d6093ed18edef4b6ded8c1
Lucas1774 <lucas.115@hotmail.com>
2024-03-03 00:12:21 +0100

    Style TODO: implement regressive search TODO: database schema TODO: implement database telecomunication

--- client/src/components/sudokuGrid.js
+++ client/src/components/sudokuGrid.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function SudokuGrid({ sudokuString }) {
     return (
-        <div>
-            {sudokuString.split('').map((digit, index) => (
-                index % 9 === 8 ? <span key={index}>{digit}<br /></span> : <span key={index}>{digit}</span>
+        <div className="sudoku-grid">
+            {sudokuString.split('').map((digit) => (
+                <span className="sudoku-cell">{digit === '0' ? '' : digit}</span>
             ))}
         </div>
     );
