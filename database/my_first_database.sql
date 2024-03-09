67e0deca5c1ce2e8bd5c66508324b01d25713604
Lucas1774 <lucas.115@hotmail.com>
2024-03-10 00:10:11 +0100

    Database schema script

--- database/my_first_database.sql
+++ database/my_first_database.sql
@@ -1,48 +1,137 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
---
--- Host: localhost    Database: my_first_database
--- ------------------------------------------------------
--- Server version	8.0.32
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
---
--- Table structure for table `my_first_table`
---
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema my_first_database
+-- -----------------------------------------------------
 
-DROP TABLE IF EXISTS `my_first_table`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `my_first_table` (
-  `column_1` int DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- -----------------------------------------------------
+-- Schema my_first_database
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `my_first_database` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `my_first_database` ;
 
---
--- Dumping data for table `my_first_table`
---
+-- -----------------------------------------------------
+-- Table `my_first_database`.`my_table`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `my_first_database`.`my_table` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ans` VARCHAR(50) NULL DEFAULT NULL,
+  `text` VARCHAR(255) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 493
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
-LOCK TABLES `my_first_table` WRITE;
-/*!40000 ALTER TABLE `my_first_table` DISABLE KEYS */;
-/*!40000 ALTER TABLE `my_first_table` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- -----------------------------------------------------
+-- Table `my_first_database`.`sudokus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `my_first_database`.`sudokus` (
+  `idsudokus` INT NOT NULL AUTO_INCREMENT,
+  `state` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`idsudokus`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 49
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
 
--- Dump completed on 2023-03-18 16:25:42
+USE `my_first_database` ;
+
+-- -----------------------------------------------------
+-- procedure get_ans
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `my_first_database`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_ans`()
+BEGIN
+	SELECT ans FROM my_table WHERE id = 1;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure get_sudokus
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `my_first_database`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_sudokus`()
+BEGIN
+	SELECT state FROM sudokus;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure get_text
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `my_first_database`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_text`()
+BEGIN
+	SELECT text FROM my_table WHERE id = 1;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure insert_sudoku
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `my_first_database`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `insert_sudoku`(
+    IN p_state VARCHAR(255)
+)
+BEGIN
+    DECLARE rowCount INT;
+    
+    SELECT COUNT(*) INTO rowCount FROM sudokus WHERE state = p_state;
+    
+    IF rowCount = 0 THEN
+        INSERT INTO sudokus (state) VALUES (p_state);
+    END IF;
+    
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure update_ans
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `my_first_database`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_ans`(IN p_number varchar(50))
+BEGIN
+	UPDATE my_table SET ans = p_number WHERE id = 1;
+END$$
+
+DELIMITER ;
+
+-- -----------------------------------------------------
+-- procedure update_text
+-- -----------------------------------------------------
+
+DELIMITER $$
+USE `my_first_database`$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_text`(IN p_string varchar(255))
+BEGIN
+	UPDATE my_table SET text = p_string WHERE id = 1;
+END$$
+
+DELIMITER ;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
