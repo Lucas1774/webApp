b1085a02cd554f2cfdfdb27bc562195b1b0ddf27
Lucas1774 <lucas.115@hotmail.com>
2024-05-06 23:01:22 +0200

    Implement mega

--- client/src/scramblers/megaScrambler.js
+++ client/src/scramblers/megaScrambler.js
@@ -1,4 +1,22 @@
+export const SCRAMBLE_LENGTH = 77;
+const SCRAMBLE_MOVES = [
+    ["R++ ", "R-- "],
+    ["D++ ", "D-- "],
+];
+const ELEVENTH = ["U \n", "U' \n"];
+
 export const Scramble = () => {
-    let scramble = "not implemented yet";
+    let scramble = "";
+    let turn = 0;
+    let turnIterator;
+    for (let i = 0; i < SCRAMBLE_LENGTH; i++) {
+        turnIterator = Math.floor(Math.random() * 2);
+        if (0 !== (i + 1) % 11) {
+            scramble += SCRAMBLE_MOVES[turn][turnIterator];
+            turn = (turn + 1) % 2;
+        } else {
+            scramble += ELEVENTH[turnIterator];
+        }
+    }
     return scramble;
 }
