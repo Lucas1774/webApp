0def8912104e39992462c09b36c02028e79d2401
Lucas1774 <lucas.115@hotmail.com>
2024-05-15 03:25:22 +0200

    Implement 5x5

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,5 +2,6 @@ export const EMPTY_TIMER = "-:--:---";
 export const TWO = "two";
 export const THREE = "three";
 export const FOUR = "four";
+export const FIVE = "five";
 export const BLD = "bld";
 export const MEGAMINX = "megaminx";
