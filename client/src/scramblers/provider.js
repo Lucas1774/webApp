77403bff0fb5d9cc160d9703bb41b2b98dc4c4ec
Lucas1774 <lucas.115@hotmail.com>
2024-05-16 13:10:25 +0200

    Implement multi

--- client/src/scramblers/provider.js
+++ client/src/scramblers/provider.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { Scramble as twoScrambler } from "../scramblers/twoScrambler";
 import { Scramble as threeScrambler } from "../scramblers/threeScrambler";
 import { Scramble as fourScrambler } from "../scramblers/fourScrambler";
@@ -5,27 +6,51 @@ import { Scramble as fiveScrambler } from "./fiveScrambler";
 import { Scramble as bldScrambler } from "../scramblers/bldScrambler";
 import { Scramble as pyraminxScrambler } from "../scramblers/pyraScrambler";
 import { Scramble as megaScrambler } from "../scramblers/megaScrambler";
-import { TWO, THREE, FOUR, FIVE, BLD, PYRAMINX, MEGAMINX } from "../constants";
+import { TWO, THREE, FOUR, FIVE, BLD, MULTI, PYRAMINX, MEGAMINX } from "../constants";
 
-const Scramble = (puzzle) => {
-    switch (puzzle) {
-        case TWO:
-            return twoScrambler();
-        case THREE:
-            return threeScrambler();
-        case FOUR:
-            return fourScrambler();
-        case FIVE:
-            return fiveScrambler();
-        case BLD:
-            return bldScrambler();
-        case PYRAMINX:
-            return pyraminxScrambler();
-        case MEGAMINX:
-            return megaScrambler();
-        default:
-            return "";
-    }
+const Scramble = (props) => {
+    const [scramble, setScramble] = useState("");
+
+    useEffect(() => {
+        if (props.new && props.display === "block") {
+            switch (props.puzzle) {
+                case TWO:
+                    setScramble(twoScrambler());
+                    break;
+                case THREE:
+                    setScramble(threeScrambler());
+                    break;
+                case FOUR:
+                    setScramble(fourScrambler());
+                    break;
+                case FIVE:
+                    setScramble(fiveScrambler());
+                    break;
+                case BLD:
+                    setScramble(bldScrambler());
+                    break;
+                case MULTI:
+                    let multiScrambles = [];
+                    for (let i = 0; i < props.quantity; i++) {
+                        multiScrambles.push(bldScrambler());
+                    }
+                    setScramble(multiScrambles.map((scramble, index) => <p key={index}>{index + 1}{")"} {scramble}</p>));
+                    break;
+                case PYRAMINX:
+                    setScramble(pyraminxScrambler());
+                    break;
+                case MEGAMINX:
+                    setScramble(megaScrambler());
+                    break;
+                default:
+                    setScramble("");
+            }
+        }
+    }, [props.new, props.display, props.puzzle, props.quantity]);
+
+    return (
+        <h2 className={props.puzzle} style={{ display: props.display }}>{scramble}</h2>
+    );
 };
 
 export default Scramble;
