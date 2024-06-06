0af8655016645f668efce3da009af3875e32998d
Lucas1774 <lucas.115@hotmail.com>
2024-06-07 00:17:31 +0200

    Remove Sudoku demo

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,12 @@ import SecretSanta from './components/secretSanta';
 import About from './components/parsedAbout';
 import Sudoku from './components/sudoku';
 import RubikTimer from './components/rubikTimer';
-import video from './assets/video/sudoku_demo.webm';
 import "./assets/styles/App.css"
 
 const App = () => {
   return (
     <><div style={{ display: "flex", flexWrap: "wrap" }}>
       <About />
-      <div className='app' style={{ margin: "auto" }}>
-        <video controls width="100%">
-          <source src={video} type="video/webm" />
-        </video>
-      </div>
       <br></br>
     </div>
       <Row>
