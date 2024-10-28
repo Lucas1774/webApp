bd454b4579a2179443b06ecf7dc58c14e6cd7582
Lucas1774 <lucas.115@hotmail.com>
2024-10-28 21:04:35 +0100

    Fix Lombok version

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sed -i 's/active: .*/active: prod/' src/main/resources/application.yml
-./mvnw.cmd clean package
+./mvnw.cmd clean package -X
 mv -f target/server-0.0.1-SNAPSHOT.jar server.jar
 sed -i 's/active: .*/active: dev/' src/main/resources/application.yml
 echo "Press any key to exit."
