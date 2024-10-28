1f97e72b6f8b02c6935387a0a2c8c268ab86adba
Lucas1774 <lucas.115@hotmail.com>
2024-10-28 16:56:28 +0100

    Implement filters and orders

--- server/src/main/java/com/lucas/server/components/model/ShoppingItem.java
+++ server/src/main/java/com/lucas/server/components/model/ShoppingItem.java
@@ -1,5 +1,7 @@
 package com.lucas.server.components.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,7 +10,10 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 public class ShoppingItem {
+    @JsonProperty("ID")
     private int id;
+    @JsonProperty("NAME")
     private String name;
+    @JsonProperty("QUANTITY")
     private int quantity;
 }
