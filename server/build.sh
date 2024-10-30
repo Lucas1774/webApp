16b1aed425e18817761ca13f01392b4d193dc722
Lucas1774 <lucas.115@hotmail.com>
2024-10-30 13:32:30 +0100

    Improve server build script

--- server/build.sh
+++ server/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sed -i 's/active: .*/active: prod/' src/main/resources/application.yml
 ./mvnw.cmd clean package -X
+if [ -f "server.jar" ]; then
+    rm -f server.jar
+fi
 mv -f target/server-0.0.1-SNAPSHOT.jar server.jar
 sed -i 's/active: .*/active: dev/' src/main/resources/application.yml
 echo "Press any key to exit."
